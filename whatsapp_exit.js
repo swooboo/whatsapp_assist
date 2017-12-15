c701f700dc75c1c093bf6e7ded9db4ff7d320f68
swooboo <swooboo@mail.ru>
2017-12-15 19:26:03 +0200

    Remove unneeded clicking functions

--- whatsapp_exit.js
+++ whatsapp_exit.js
@@ -12,9 +12,3 @@ var downEvent = generateEvent("mousedown");
 
 // Open chat menu after selecting the chat
 function clickChatMenu(){ $(".pane-chat-controls > div:nth-child(1) > div:nth-child(2) > button:nth-child(1)").click(); };
-
-// Clicking group info button after opening menu
-function clickGroupInfo() { $("li.menu-item:nth-child(1) > a:nth-child(1)").dispatchEvent(upEvent); };
-
-// Click chat number #NUM
-function clickChatNo(num){ $(".infinite-list-viewport > div:nth-child(" + num + ") div.chat").dispatchEvent(downEvent); }
