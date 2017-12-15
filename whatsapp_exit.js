675f279a6764faf8c522c8d28d9458ab48ad2682
swooboo <swooboo@mail.ru>
2017-12-15 19:31:30 +0200

    Add clickTargetChat() function

--- whatsapp_exit.js
+++ whatsapp_exit.js
@@ -13,3 +13,23 @@ var allChats = Array.from(document.querySelectorAll(".chat"));
 
 // Open chat menu after selecting the chat
 function clickChatMenu(){ document.querySelector("div.pane-chat-controls div[title='Menu']").dispatchEvent(downEvent); };
+
+// Click the target chat
+function clickTargetChat(targetChatTitle){
+	console.log("Targeting chat title '" + targetChatTitle +"'");
+	
+	// Will take the first one that has a title match
+	targetChat = allChats.filter(function(el){
+		return el.querySelector(".chat-body>.chat-main>.chat-title>.emojitext").innerText.match(targetChatTitle);
+	})[0];
+
+	if(!targetChat){	// Didn't find a matching chat
+		console.log("Couldn't locate chat '" + targetChatTitle + "'");
+		return false;
+	}
+	else {
+		console.log("Located the chat: ", targetChat);
+		targetChat.dispatchEvent(downEvent);
+		return true;
+	}
+}
